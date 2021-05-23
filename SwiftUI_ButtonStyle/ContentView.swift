9e92ca7da74c9d54a5bd2524583249e0f4cf0329
sudoswift <sudoswift@gmail.com>
2021-05-23 18:43:26 +0900

    Initial Commit

---
+++ SwiftUI_ButtonStyle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_ButtonStyle
+//
+//  Created by izvern on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_ButtonStyle/SwiftUI_ButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ButtonStyleApp.swift
+//  SwiftUI_ButtonStyle
+//
+//  Created by izvern on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
